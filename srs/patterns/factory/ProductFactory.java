15f57e25e62ce9bf29c25dd532698cf3aa093345
ars02280 <spakspak01@gmail.com>
2026-02-09 00:06:26 +0500

    patterns

--- srs/patterns/factory/ProductFactory.java
+++ srs/patterns/factory/ProductFactory.java
@@ -1,9 +1,13 @@
-public class ProductFactory {
+package patterns.factory;
+
+import model.BaseEntity;
+
+ class ProductFactory {
 
     public static BaseEntity create(String type, Long id, String name, double value) {
         return switch (type.toUpperCase()) {
-            case "DIGITAL" -> new DigitalProduct(id, name, value);
-            case "PHYSICAL" -> new PhysicalProduct(id, name, value);
+            case "GAME" -> new GameProduct(id, name, value);
+            case "APP" -> new AppProduct(id, name, value);
             default -> throw new IllegalArgumentException("Unknown product type");
         };
     }
